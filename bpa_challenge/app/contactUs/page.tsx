f8e40b42101e36f49d675b6afdb560831659f38a
kvgreat14637 <kvaranasi9@gmail.com>
2024-11-10 15:17:57 -0600

    Update page.tsx
    Added HTML for Contact Us page

--- bpa_challenge/app/contactUs/page.tsx
+++ bpa_challenge/app/contactUs/page.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <div class="contact-form">
+    <h2>Contact Us</h2>
+    <form id="contactForm">
+        <input type="text" id="name" placeholder="Name" required>
+        <input type="email" id="email" placeholder="Email" required>
+        <textarea id="message" rows="4" placeholder="Message" required></textarea>
+        <button type="button" onclick="submitForm()">Submit</button>
+    </form>
+    <p id="responseMessage"></p>
+</div>
   )
 }
 
